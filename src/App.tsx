41cc9de618829578dd68e491307e64bb6e9b241f
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-01-17 22:32:06 +0100

    design changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import Sidebar from "./components/Sidebar";
-
-import Header from "./components/Header";
 import Board from "./components/Board";
+import { BoardsComponent } from "./components/Boards";
+
+const testColumns1 = [
+  { title: "todo", iconColor: "red", items: ["do loundry", "go to the gym"] },
+  { title: "doing", iconColor: "yellow", items: ["do homework"] },
+  { title: "done", iconColor: "green", items: ["wash dishes"] },
+];
+
+const testColumns2 = [
+  { title: "todo2", iconColor: "red", items: ["do loundry", "go to the gym"] },
+  { title: "doing2", iconColor: "yellow", items: ["do homework"] },
+  { title: "done2", iconColor: "green", items: ["wash dishes"] },
+];
 
 function App() {
+  const boards = [
+    { label: "Tab 1", content: <Board columnsData={testColumns1} /> },
+    { label: "Tab 2", content: <div>Content for Tab 2</div> },
+    { label: "Tab 3", content: <div>Content for Tab 3</div> },
+  ];
+
   return (
     <div className="bg-slate-800">
-      <div className="max-w-7xl min-h-screen mx-auto flex  text-zinc-100">
-        <Sidebar />
-        <div className="my-8 mr-4 w-full">
-          <Header />
-          <Board />
-        </div>
+      <div className="min-h-screen mx-auto max-w-7xl text-zinc-100">
+        <BoardsComponent boards={boards} />
       </div>
     </div>
   );
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,106 +1,63 @@
 import { Circle, PlusSquare } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./ui/modal";
+import AddColumnModal from "./add-column";
 
-const testColumns = [
-  { title: "todo", iconColor: "red", items: ["do loundry", "go to the gym"] },
-  { title: "doing", iconColor: "yellow", items: ["do homework"] },
-  { title: "done", iconColor: "green", items: ["wash dishes"] },
-];
-
-function Board() {
-  const [columns, setColumns] = useState(testColumns);
+function Board({ columnsData }: any) {
+  const [columns, setColumns] = useState(columnsData);
   const [open, setOpen] = useState(false);
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("");
 
-  function getColorValue(e: any) {
-    setColor(e.target.id.toString());
-  }
-  const handleChange = (event: any) => {
-    setTitle(event.target.value);
-  };
-  const handleSubmit = (event: any) => {
-    if (title) {
-      const newColumn = {
-        title: title,
-        iconColor: color,
-        items: ["test", "test2"],
-      };
-      setColumns(columns.concat(newColumn));
+  useEffect(() => {
+    localStorage.setItem("columns", JSON.stringify(columns));
+  }, [columns]);
+
+  useEffect(() => {
+    if (localStorage.hasOwnProperty("columns")) {
+      // const columns = JSON.parse(localStorage.getItem("columns"));
+      if (columns) {
+        setColumns(columns);
+      }
+    } else {
+      console.log(columns);
     }
-    event.preventDefault();
-    setOpen(false);
-  };
+  }, []);
+
   return (
-    <div className="flex h-1/2 rounded-lg my-4 gap-2 p-8 border border-zinc-400">
-      {columns.map((column) => {
+    <div className="flex h-full gap-2 p-4 border rounded-lg border-zinc-400">
+      {columns.map((column: any) => {
         return (
           <div
             key={column.title}
-            className="border flex flex-col  border-zinc-300 p-2"
+            className="flex flex-col p-2 border h-fit border-zinc-400"
           >
             <h3 className="flex gap-2">
               <Circle color={column.iconColor} />
               {column.title} ({column.items.length})
             </h3>
             <ul>
-              {column.items.map((item) => {
+              {column.items.map((item: any) => {
                 return <li key={item}>- {item}</li>;
               })}
             </ul>
+            <button className="flex items-center gap-2 p-2 rounded-lg bg-slate-700 hover:bg-slate-700/80">
+              <PlusSquare /> Add
+            </button>
           </div>
         );
       })}
 
       <button
         onClick={() => setOpen(true)}
-        className="flex items-center bg-slate-700 hover:bg-slate-700/80 gap-2 p-2 rounded-lg"
+        className="flex items-center gap-2 p-2 rounded-lg bg-slate-700 hover:bg-slate-700/80"
       >
         <PlusSquare /> Add Column
       </button>
       <Modal open={open} onClose={() => setOpen(false)}>
-        <div className="p-2 mt-8 w-56">
-          <form onSubmit={handleSubmit} className="text-black">
-            <label htmlFor="title">Title:</label>
-            <input
-              onChange={handleChange}
-              value={title}
-              className="border border-gray-400 px-2"
-              type="text"
-              placeholder="title"
-            />
-            Color:
-            <div className="flex m-2 gap-2 items-center justify-center">
-              <input
-                onClick={getColorValue}
-                className="bg-red-600 w-4 h-4 rounded-full inline-block cursor-pointer focus:outline-none focus:ring focus:ring-black"
-                id="red"
-              ></input>
-              <input
-                onClick={getColorValue}
-                className="bg-blue-600 w-4 h-4 rounded-full inline-block cursor-pointer focus:outline-none focus:ring focus:ring-black"
-                id="blue"
-              ></input>
-              <input
-                onClick={getColorValue}
-                className="bg-green-600 w-4 h-4 rounded-full inline-block cursor-pointer focus:outline-none focus:ring focus:ring-black"
-                id="green"
-              ></input>
-              <input
-                onClick={getColorValue}
-                className="bg-yellow-400 w-4 h-4 rounded-full inline-block cursor-pointer focus:outline-none focus:ring focus:ring-black"
-                id="yellow"
-              ></input>
-            </div>
-            <button
-              type="submit"
-              className="flex w-full justify-center hover:bg-slate-700/80 gap-2 py-2 rounded-lg"
-            >
-              <PlusSquare /> Add
-            </button>
-          </form>
-        </div>
+        <AddColumnModal
+          setColumns={setColumns}
+          columns={columns}
+          setOpen={setOpen}
+        />
       </Modal>
     </div>
   );
